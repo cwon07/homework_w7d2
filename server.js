90e1170611821c37bc2408c9c9418dee0d5b7022
Christine Wong <jumiryoo@gmail.com>
2023-06-08 23:46:51 -0700

    set up the server

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const express = require("express")
 const app = express()
 
 const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send(`Welcome to the Gitpub App!`)
+})
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})

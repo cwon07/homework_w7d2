4874e3722360a53a908470904d1aadde1e0fdb38
Christine Wong <jumiryoo@gmail.com>
2023-06-09 00:10:16 -0700

    set up the index view

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-
 const PORT = process.env.PORT
+const drinks = require('./models/drinks')
 
 app.get('/', (req, res) => {
     res.send(`Welcome to the Gitpub App!`)
+})
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
 })
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)

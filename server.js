547b66a1af2657eea5d31252925d323c3c73ef4b
Christine Wong <jumiryoo@gmail.com>
2023-06-13 19:20:06 -0700

    rendered individual drinks in the show view

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const drinks = require('./models/drinks')
 
@@ -20,7 +20,10 @@ app.get('/drinks', (req, res) => {
 
 // shows one drink - drinks/:id
 app.get('/drinks/:id', (req, res) => {
-    res.send(req.params.id)
+    const id = req.params.id
+    const drink = drinks[id]
+    // console.log('drink:', drink);
+    res.render("show.ejs", {drink})
  })
 
 app.listen(PORT, () => {
